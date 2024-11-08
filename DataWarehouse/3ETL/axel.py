51bc5ef38124a8853349fde31fac712989c3a046
Escalante Axel Joel <axelescalante0@gmail.com>
2024-11-07 22:43:30 -0300

    Tabla de hechos ya solucionada

--- DataWarehouse/3ETL/axel.py
+++ DataWarehouse/3ETL/axel.py
@@ -4,35 +4,28 @@
 import psycopg2
 import pandas as pd
 from sqlalchemy import create_engine # Importante para la conexion con la base de datos
-from Modulos.update_dimensions_table import actualizarTablaDimension
+import os
+from modulos.update_dimensions_table import actualizarTablaDimension
 
 # Conexion con la base de datos
 conn = psycopg2.connect(
     host="localhost",
-    database="Lavarropas",
+    database="Lavarropas2",
     user="postgres",
-    password="1234",
-    options='-c client_encoding=UTF8'
+    password="1234"
 )
 
-# realizamos una consulta para ver los datos de la tabla
-cur = conn.cursor()
-cur.execute("""SELECT 
-    ID_Ciclo,
-    SUM(Consumo_energia) AS Consumo_total_energia,
-    SUM(Consumo_agua) AS Consumo_total_agua
-FROM 
-    Consumo_lavarropas
-GROUP BY 
-    ID_Ciclo;""")
-rows = cur.fetchall()
-print(rows)
+
+
 
 # conexion con el datawarehouse
 engine = create_engine('postgresql://postgres:1234@localhost:5432/Lavarropas_DW')
 
+
+
+
 #guardamos las tablas de la base de datos relacional que utilizaremos en dataframes
-listas_tablas = ['Consumo_lavarropas', 'Ciclo_lavado', 'lavarropas','fase','marca','programa', 'ubicacion', 'usuarios' ]
+listas_tablas = ['Consumo_lavarropas', 'Ciclo_lavado', 'Lavarropas','fase','marca','programa', 'ubicacion', 'usuarios' ]
 
 tablas = {}
 for tabla in listas_tablas:
@@ -45,13 +38,11 @@ conn.close()
 #generamos consulta sql de la base de datos relacional de la tabla ubicaion para luego cargarla en el datawarehouse
 # Consulta sql para obtener los datos de la tabla Ubicacion
 
-
-
-
 #---------------------
 #dimension ubicaion
 #---------------------
 ubicacion = tablas['ubicacion']
+ubicacion 
 print(ubicacion)
 
 #actualizamos la tabla de ubicacion
@@ -77,11 +68,17 @@ tiempo['mes'] = tiempo['fecha_inicio'].dt.month
 tiempo['dia'] = tiempo['fecha_inicio'].dt.day
 tiempo['hora'] = tiempo['fecha_inicio'].dt.hour
 
+
+
+# Generar id_fecha único
+
 print(tiempo)
 
-# Cargo la tabla en el datawarehouse
-tiempo = actualizarTablaDimension(engine, 'tiempo', tiempo, pk=['anio', 'mes', 'dia', 'hora'])
+#cargo la tabla en el datawarehouse
+tiempo = actualizarTablaDimension(engine,'tiempo', tiempo, pk='id_fecha')
 
+tiempo= pd.read_sql_table('tiempo', engine)
+print(tiempo)
 #JOIN entre las tablas de la base de datos relacional 
 
 #--------------------
@@ -89,14 +86,14 @@ tiempo = actualizarTablaDimension(engine, 'tiempo', tiempo, pk=['anio', 'mes', '
 #--------------------
 fact_table = pd.DataFrame({
     #Dimensiones
-    'id_tiempo': tablas['Ciclo_lavado']['id_ciclo'].astype(int), #tiempo['fecha_inicio'].map(tiempo.set_index('fecha_inicio')['id_fecha'])
+    'id_tiempo': tiempo['id_fecha'].astype(int),
     'id_ubicacion': tablas['ubicacion']['id_ubicacion'].astype(int),
     'id_marca': tablas['marca']['id_marca'].astype(int),
 
     #Medidas
     'numero_lavados' : tablas['Ciclo_lavado']['id_ciclo'].groupby(tablas['Ciclo_lavado']['id_ciclo']).count(),
-    'consumo_total_energia_kwh': tablas['Consumo_lavarropas'].groupby('id_ciclo')['consumo_energia'].sum(),
-    'consumo_total_agua_l': tablas['Consumo_lavarropas'].groupby('id_ciclo')['consumo_agua'].sum()
+    'consumo_total_energia_kwh': tablas['Consumo_lavaropas'].groupby('id_ciclo')['consumo_energia'].sum(),
+    'consumo_total_agua_l': tablas['Consumo_lavaropas'].groupby('id_ciclo')['consumo_agua'].sum()
 
 
 })
